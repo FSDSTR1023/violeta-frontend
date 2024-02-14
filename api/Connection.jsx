4d4d278f1deb27a84a4f28c481403ba2990f5b66
alexander-lh <129536028+alexander-lh@users.noreply.github.com>
2024-02-14 21:14:12 +0100

    Update Connection.jsx

--- api/Connection.jsx
+++ api/Connection.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: 'https://violeta-backend.onrender.com',
+	// baseURL: 'https://violeta-backend.onrender.com',
+	baseURL: 'http://localhost:4000',
 	withCredentials: true,
 });
 

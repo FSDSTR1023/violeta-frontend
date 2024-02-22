9972335bf3061bb1ee9895aa5ad66f067e383cf0
lestrange41 <joel.montilla.heredia@gmail.com>
2024-02-22 17:39:04 +0100

    logo navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSession } from '../contexts/SessionContext';
+import TrailNestPhoto from '../img/TrailNest-logos_white.png'
 
 const Navbar = () => {
   const { getProfile, profile, closeSession } = useSession();
@@ -36,8 +37,9 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-800 text-white p-4 flex justify-between items-center">
       <div className="flex items-center">
-        <img src="https://res.cloudinary.com/trailnestcloud/image/upload/logo/v1707510978/67da092fba73d78ddc9f74d7db185575_hllda5.jpg" alt="Logo" className="w-8 mr-2 rounded-xl" />
-        <Link to='/' className="text-xl font-semibold">Trailnest</Link>
+       <Link to='/' className="text-xl font-semibold">
+      <img src={TrailNestPhoto} alt="Logo" className="w-22 h-10 mr-2 rounded-xl cursor-pointer" />
+       </Link>
       </div>
       <ul className="flex gap-4">
         <li><Link to="/rutas" className="hover:text-gray-300">Rutas</Link></li>

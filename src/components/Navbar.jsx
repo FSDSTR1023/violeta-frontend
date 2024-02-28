ffd304d3dc8f28f8123cf96f4f78b7824e4a8600
Pablo Labarta <plabartalarrauri@gmail.com>
2024-02-28 19:09:25 +0000

    switching menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSession } from '../contexts/SessionContext';
-import TrailNestPhoto from '../img/TrailNest-logos_white.png'
+import TrailNestPhoto from '../img/TrailNest-logos_white.png';
 
 const Navbar = () => {
   const { getProfile, profile, closeSession } = useSession();
@@ -35,22 +35,23 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-gray-800 text-white p-4 flex justify-between items-center">
-      <div className="flex items-center">
-       <Link to='/' className="text-xl font-semibold">
-      <img src={TrailNestPhoto} alt="Logo" className="w-22 h-10 mr-2 rounded-xl cursor-pointer" />
-       </Link>
+    <nav className="bg-gray-800 text-white p-4 flex justify-between items-center ">
+      <div className="flex items-center ">
+        <Link to='/' className="text-xl font-semibold">
+          <img src={TrailNestPhoto} alt="Logo" className="w-22 h-10 mr-2 rounded-xl cursor-pointer" />
+        </Link>
+        <ul className="flex gap-4 pl-8"> {/* Padding left increased */}
+          {/* <li><Link to="/users" className="hover:text-gray-300">Usuarios</Link></li> */} {/* Link removed */}
+        </ul>
       </div>
       <ul className="flex gap-4">
-        <li><Link to="/rutas" className="hover:text-gray-300">Rutas</Link></li>
-        
         {profile ? (
           <Dropdown />
         ) : (
           <>
-            <li><Link to="/users" className="hover:text-gray-300">Usuarios</Link></li>
-            <li><Link to="/login" className="hover:text-gray-300">Acceder</Link></li>
-            <li><Link to="/signup" className="hover:text-gray-300">Registrarse</Link></li>
+            <li><Link to="/rutas" className="hover:text-gray-300">Rutas</Link></li>
+            <li><Link to="/login" className="hover:text-gray-300">Iniciar sesión</Link></li>
+            <li><Link to="/signup" className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full shadow-md">Registrarse</Link></li>
           </>
         )}
       </ul>

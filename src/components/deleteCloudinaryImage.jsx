06b25bb047e2fb86c5bf1f4c14de022e71255a57
alexander-lh <lasherasalexander@gmail.com>
2024-02-28 20:57:53 +0100

    rename

--- src/components/deleteCloudinaryImage.jsx
+++ src/components/deleteCloudinaryImage.jsx
@@ -1,24 +1,26 @@
 import axios from "axios";
 
-const instanceaxios = axios.create({
-  withCredentials: true,
-});
-
-const deleteCloudinaryImage = async (publicId, folder) => {
+const DeleteCloudinaryImage = async (publicId, folder) => {
   try {
     const cloudinaryParams = {
-      public_id: folder ? `${folder}/${publicId}` : publicId,
+      folder: folder,
+      public_id: publicId,
       api_key: import.meta.env.VITE_CLOUDINARY_APIKEY,
       api_secret: import.meta.env.VITE_CLOUDINARY_APISECRET,
     };
+    console.log(cloudinaryParams);
     const cloudinaryUrl = `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_NAME}/image/destroy`;
 
-    await instanceaxios.delete(cloudinaryUrl, {}, { params: cloudinaryParams });
+    const response = await axios.delete(cloudinaryUrl, { params: cloudinaryParams });
 
-    console.log(`Cloudinary image with public ID ${publicId} deleted`);
+    if (response.data.result === 'ok') {
+      console.log(`Cloudinary image with public ID ${publicId} deleted`);
+    } else {
+      console.error(`Failed to delete Cloudinary image`, response.data);
+    }
   } catch (error) {
     console.error(`Error deleting Cloudinary image`, error);
   }
 };
 
-export default deleteCloudinaryImage;
+export default DeleteCloudinaryImage;

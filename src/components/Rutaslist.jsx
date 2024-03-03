67defbdf5a41c63133c8a794849f836d924df343
alexander-lh <lasherasalexander@gmail.com>
2024-03-03 21:35:55 +0100

    improved visually

--- src/components/Rutaslist.jsx
+++ src/components/Rutaslist.jsx
@@ -24,36 +24,39 @@ function ListAllRutas() {
 
   return (
     <div className="container mx-auto">
-      <h2 className="text-2xl font-bold mb-4  text-center mt-8">Rutas:</h2>
-      <div className="flex flex-wrap space-x-4 gap-2">
+      <h2 className="text-2xl font-bold mb-4 text-center mt-8">Rutas:</h2>
+      <div className="space-y-4">
         {rutas.map((ruta) => (
-          <div key={ruta._id} className="bg-slate-50 rounded-lg shadow-md p-4 w-72">
-            <p className="text-lg font-semibold mb-2 gap-1">
-              <span className="text-gray-400 capitalize">Name: </span>
-              <button
-                onClick={() => handleRutaClick(ruta._id)}
-                className="underlined cursor-pointer"
-              >
-                {ruta.name}
-              </button>
-            </p>
-            {Object.entries(ruta).map(([key, value]) => (
-              !excludedProperties.includes(key) && key !== 'name' && (
-                <p key={key} className="text-lg font-semibold mb-2 gap-1">
-                  <span className="text-gray-400 capitalize">{key}: </span>
-                  {key === 'imageUrl' ? (
-                    <img src={value} alt="Cloudinary Image" className="w-52 h-52" />
-                  ) : (
+          <div key={ruta._id} className="bg-slate-50 rounded-lg shadow-md p-4 flex items-center">
+            <div className="flex-1">
+              <p className="text-lg font-semibold mb-2 gap-1">
+                <span className="text-gray-400 capitalize">Name: </span>
+                <button
+                  onClick={() => handleRutaClick(ruta._id)}
+                  className="underlined cursor-pointer"
+                >
+                  {ruta.name}
+                </button>
+              </p>
+              {Object.entries(ruta).map(([key, value]) => (
+                !excludedProperties.includes(key) && key !== 'name' && key !== 'imageUrl' && (
+                  <p key={key} className="text-lg font-semibold mb-2 gap-1">
+                    <span className="text-gray-400 capitalize">{key}: </span>
                     <span className="underlined">{value}</span>
-                  )}
-                </p>
-              )
-            ))}
+                  </p>
+                )
+              ))}
+            </div>
+            {ruta.imageUrl && (
+              <img src={ruta.imageUrl} alt="Cloudinary Image" className="w-52 h-52 ml-4" />
+            )}
           </div>
         ))}
       </div>
     </div>
-  );  
+  );
+  
+    
 }
 
 export default ListAllRutas;

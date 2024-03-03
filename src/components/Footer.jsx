3bbe906e95f989a4f7dca0dc8d553afebb897f6f
alexander-lh <lasherasalexander@gmail.com>
2024-03-03 21:36:04 +0100

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,19 +3,8 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+    <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center justify-around px-4 md:px-6 border-t">
       <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 Mountain Routes. All rights reserved.</p>
-      <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-        <Link to="/about" className="text-xs hover:underline underline-offset-4">
-          About
-        </Link>
-        <Link to="/contact" className="text-xs hover:underline underline-offset-4">
-          Contact
-        </Link>
-        <Link to="/terms" className="text-xs hover:underline underline-offset-4">
-          Terms of Service
-        </Link>
-      </nav>
       <div className="flex gap-4">
         <a href="#" className="text-xs hover:underline underline-offset-4">Facebook</a>
         <a href="#" className="text-xs hover:underline underline-offset-4">X</a>

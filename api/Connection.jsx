166e3b67a40c62cf0b908fd66cec102f51fb152e
alexander-lh <lasherasalexander@gmail.com>
2024-03-08 11:58:06 +0100

    production down

--- api/Connection.jsx
+++ api/Connection.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: 'https://violeta-backend.onrender.com',
-	// baseURL: 'http://localhost:4000',
+	// baseURL: 'https://violeta-backend.onrender.com',
+	baseURL: 'http://localhost:4000',
 	withCredentials: true,
 });
 
